84cd10d338720f287f5b2493d9721487e0e07ba2
deshraj <deshrajdry@gmail.com>
2014-08-20 11:37:18 +0530

    views added for the 24 pages

--- kidsapp/kids/views.py
+++ kidsapp/kids/views.py
@@ -25,12 +25,75 @@ def level4(request):
 def parallax(request):
 	return render_to_response('parallax.html', context_instance=RequestContext(request))
 
-	# return HttpResponse("Coming soon level4 page !!!!")
+def level11(request):
+	return render_to_response('1_1.html', context_instance=RequestContext(request))
 
+def level12(request):
+	return render_to_response('1_2.html', context_instance=RequestContext(request))
 
-# def home(request):    
-#     """ landing page. """
-#     c = {}
-#     c.update(csrf(request))
-#     return render_to_response('index.html', context_instance=RequestContext(request))
-# # 	return HttpResponse("This page is under construction")
+def level13(request):
+	return render_to_response('1_3.html', context_instance=RequestContext(request))
+
+def level14(request):
+	return render_to_response('1_4.html', context_instance=RequestContext(request))
+
+def level21(request):
+	return render_to_response('2_1.html', context_instance=RequestContext(request))
+
+def level22(request):
+	return render_to_response('2_2.html', context_instance=RequestContext(request))
+
+def level23(request):
+	return render_to_response('2_3.html', context_instance=RequestContext(request))
+
+def level24(request):
+	return render_to_response('2_4.html', context_instance=RequestContext(request))
+
+def level31(request):
+	return render_to_response('3_1.html', context_instance=RequestContext(request))
+
+def level32(request):
+	return render_to_response('3_2.html', context_instance=RequestContext(request))
+
+def level33(request):
+	return render_to_response('3_3.html', context_instance=RequestContext(request))
+
+def level34(request):
+	return render_to_response('3_4.html', context_instance=RequestContext(request))
+
+def level41(request):
+	return render_to_response('4_1.html', context_instance=RequestContext(request))
+
+def level42(request):
+	return render_to_response('4_2.html', context_instance=RequestContext(request))
+
+def level43(request):
+	return render_to_response('4_3.html', context_instance=RequestContext(request))
+
+def level44(request):
+	return render_to_response('4_4.html', context_instance=RequestContext(request))
+
+def level51(request):
+	return render_to_response('5_1.html', context_instance=RequestContext(request))
+
+def level52(request):
+	return render_to_response('5_2.html', context_instance=RequestContext(request))
+
+def level53(request):
+	return render_to_response('5_3.html', context_instance=RequestContext(request))
+
+def level54(request):
+	return render_to_response('5_4.html', context_instance=RequestContext(request))
+
+def level61(request):
+	return render_to_response('6_1.html', context_instance=RequestContext(request))
+
+def level62(request):
+	return render_to_response('6_2.html', context_instance=RequestContext(request))
+
+def level63(request):
+	return render_to_response('6_3.html', context_instance=RequestContext(request))
+
+def level64(request):
+	return render_to_response('6_4.html', context_instance=RequestContext(request))
+
